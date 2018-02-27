33c66d0a882de328acf567761d9ee30a930fd0a4
Alexandr Gergalo <derien001@gmal.com>
2018-02-28 00:58:54 +0200

    Написали процедуру определения числа как палиндром

--- Palindrom_of_primes/Program.cs
+++ Palindrom_of_primes/Program.cs
@@ -10,6 +10,27 @@ namespace Palindrom_of_primes
     {
         static void Main(string[] args)
         {
+
+        }
+
+        //Check that number is palindrome
+        public static bool isPalindorme(long number)
+        {
+            long revNumber = 0;													//Reverse number
+            long tmpNumber = number;                                            //Number for action
+
+			//Reverse digits
+			while (tmpNumber > 10)
+			{
+				revNumber *= 10;
+				revNumber += tmpNumber % 10;
+				tmpNumber = tmpNumber / 10;
+			}
+
+			//Add last digit
+			revNumber = revNumber * 10 + tmpNumber;
+
+			return (revNumber == number) ? true:  false;
         }
     }
 }
