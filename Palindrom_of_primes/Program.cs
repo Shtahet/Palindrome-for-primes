e932e05eeea7656d12f9981faed6c0ff247f8763
Alexandr Gergalo <derien001@gmal.com>
2018-02-28 02:43:09 +0200

    Поиск палиндрома

--- Palindrom_of_primes/Program.cs
+++ Palindrom_of_primes/Program.cs
@@ -15,9 +15,9 @@ namespace Palindrom_of_primes
 			const int max5Digit = 99999;
 			const int min5Digit = 10000;
 
-			int[] intNums = Enumerable.Range(0, max5Digit).ToArray();
-			intNums[1] = 0;
-			for(int i = 2; i < intNums.Length; ++i)
+			int[] intNums = Enumerable.Range(0, max5Digit).ToArray();							//Generate array
+			intNums[1] = 0;																		//Prepare to Eratosthenes
+			for(int i = 2; i < intNums.Length; ++i)                                             //Main algorithm
 			{
 				if (intNums[i] != 0) {
 					for (int j = i * 2; j < intNums.Length; j += i)
@@ -25,10 +25,28 @@ namespace Palindrom_of_primes
 				}
 			}
 
-			int[] primes = intNums.Where(x => x >= min5Digit && x <= max5Digit).ToArray();
+			int[] primes = intNums.Where(d => d >= min5Digit && d <= max5Digit).ToArray();      //Select only 5 digit numbers
 
-			foreach (int i in primes)
-				Console.WriteLine($"{i}\t");
+			//Looking for max polindrom
+			int x = 0;                                                                          //1st multiplier
+			int y = 0;                                                                          //2nd multiplier
+			long pal = 0;									                                   //result
+
+			for(int i = 0; i < primes.Length - 1; ++i)
+				for(int j = i+1; j < primes.Length; ++j)
+				{
+					long mult = (long)primes[i] * (long)primes[j];
+					if (isPalindorme(mult) && mult > pal)
+					{
+						pal = mult;
+						x = primes[i];
+						y = primes[j];
+					}
+
+				}
+					
+			
+			Console.WriteLine($"{x} * {y} = {pal}");
 			Console.ReadLine();
 		}
 
