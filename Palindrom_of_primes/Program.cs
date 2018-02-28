a8b1a07c060f331c0e0361e3de4a09e3f0ca697b
Alexandr Gergalo <derien001@gmal.com>
2018-02-28 02:18:52 +0200

    Создали масив простых 5-ти значных чисел

--- Palindrom_of_primes/Program.cs
+++ Palindrom_of_primes/Program.cs
@@ -10,11 +10,30 @@ namespace Palindrom_of_primes
     {
         static void Main(string[] args)
         {
+			//Looking for primes 
+			//Sieve of Eratosthenes
+			const int max5Digit = 99999;
+			const int min5Digit = 10000;
 
-        }
+			int[] intNums = Enumerable.Range(0, max5Digit).ToArray();
+			intNums[1] = 0;
+			for(int i = 2; i < intNums.Length; ++i)
+			{
+				if (intNums[i] != 0) {
+					for (int j = i * 2; j < intNums.Length; j += i)
+						intNums[j] = 0;
+				}
+			}
 
-        //Check that number is palindrome
-        public static bool isPalindorme(long number)
+			int[] primes = intNums.Where(x => x >= min5Digit && x <= max5Digit).ToArray();
+
+			foreach (int i in primes)
+				Console.WriteLine($"{i}\t");
+			Console.ReadLine();
+		}
+
+		//Check that number is palindrome
+		public static bool isPalindorme(long number)
         {
             long revNumber = 0;													//Reverse number
             long tmpNumber = number;                                            //Number for action
